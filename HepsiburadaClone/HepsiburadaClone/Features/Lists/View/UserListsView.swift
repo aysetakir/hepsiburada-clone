105f86fcc17dc2e2dd5f1622f633581cf43f729b
Ayşegül Takır <aysetakir@outlook.com>
2026-06-08 16:49:52 +0300

    feat: add UserListsView and FavoritesView with mock data

--- HepsiburadaClone/HepsiburadaClone/Features/Lists/View/UserListsView.swift
+++ HepsiburadaClone/HepsiburadaClone/Features/Lists/View/UserListsView.swift
@@ -5,26 +5,44 @@ struct UserListsView: View {
     @State private var viewModel = ListsViewModel()
     
     var body: some View {
-        
-        VStack(alignment: .leading){
-            
-            
-            
-            Text("Beğendiklerim")
-                .font(.headline)
-                .underline(color: .orange)
-            
-            
-            let columns = [GridItem(.flexible(), spacing: 25), GridItem(.flexible(), spacing: 25)]
-            LazyVGrid(columns: columns, spacing: 12) {
-                ForEach(viewModel.favoriteProducts) { product in
-                    NavigationLink(destination: ProductDetailView(product: product)) {
-                        ProductCardView(product: product)
+        ScrollView {
+            VStack(alignment: .leading) {
+                
+                if viewModel.userLists.isEmpty {
+                    VStack(alignment: .center) {
+                        Text("Henüz bir liste oluşturmadın.")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .padding()
+                        
+                        Text("Ekranın sağ altında yer alan Yeni liste butonuna ")
+                        Text("dokunarak ilk listeni oluşturabilirsin")
                     }
                 }
-                .foregroundStyle(.black)
+                else {
+                    ForEach(viewModel.userLists) { list in
+                        VStack(alignment: .leading) {
+                            
+                            Text(list.name)
+                                .fontWeight(.bold)
+                                .padding(.horizontal)
+                            
+                            let columns = [GridItem(.flexible(), spacing: 12), GridItem(.flexible(), spacing: 12)]
+                            LazyVGrid(columns: columns, spacing: 12) {
+                                ForEach(list.products) { product in
+                                    NavigationLink(destination: ProductDetailView(product: product)) {
+                                        ProductCardView(product: product)
+                                    }
+                                    .foregroundStyle(.black)
+                                }
+                            }
+                            .padding(.horizontal)
+                            
+                        }
+                    }
+                    
+                }
             }
-            
         }
     }
 }
